56d4021687120e5c40115b4c6ca2a8a5271840d8
tofarr <tofarr@gmail.com>
2026-02-27 12:47:41 +0000

    Lint fixes

--- enterprise/server/auth/token_manager.py
+++ enterprise/server/auth/token_manager.py
@@ -103,14 +103,17 @@ class TokenManager:
     async def get_keycloak_tokens(
         self, code: str, redirect_uri: str
     ) -> tuple[str | None, str | None]:
-        logger.info("trace_get_keycloak_tokens")
+        logger.info('trace_get_keycloak_tokens')
         try:
             keycloak_openid = get_keycloak_openid(self.external)
-            token_response = await asyncio.wait_for(keycloak_openid.a_token(
-                grant_type='authorization_code',
-                code=code,
-                redirect_uri=redirect_uri,
-            ), 15)
+            token_response = await asyncio.wait_for(
+                keycloak_openid.a_token(
+                    grant_type='authorization_code',
+                    code=code,
+                    redirect_uri=redirect_uri,
+                ),
+                15,
+            )
 
             logger.debug(f'token_response: {token_response}')
 
