4f462e9b1707e1cad8d0c2fed838abfc60b2fab6
XDien <xdien@gmail.com>
2026-04-12 12:05:15 +0000

    fix: use absolute path for OPENHANDS_RESOLVER_TEMPLATES_DIR

--- enterprise/integrations/utils.py
+++ enterprise/integrations/utils.py
@@ -103,9 +103,11 @@ ENABLE_V1_GITLAB_RESOLVER = (
     os.getenv('ENABLE_V1_GITLAB_RESOLVER', 'false').lower() == 'true'
 )
 
+# Get the absolute path to the templates directory
+# This file is at enterprise/integrations/utils.py, so we need to go up 2 levels to enterprise/, then to openhands/
 OPENHANDS_RESOLVER_TEMPLATES_DIR = (
     os.getenv('OPENHANDS_RESOLVER_TEMPLATES_DIR')
-    or 'openhands/integrations/templates/resolver/'
+    or os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'openhands/integrations/templates/resolver/')
 )
 jinja_env = Environment(loader=FileSystemLoader(OPENHANDS_RESOLVER_TEMPLATES_DIR))
 
